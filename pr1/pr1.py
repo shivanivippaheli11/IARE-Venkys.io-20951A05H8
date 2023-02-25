4ed3247a9ea619fc9503c7714183a4e0841ecbe7
shivanivippaheli11 <104315622+shivanivippaheli11@users.noreply.github.com>
2023-02-25 11:29:26 +0530

    Create pr1.py

---
+++ pr1/pr1.py
@@ -0,0 +1,7 @@
+x=int(input("enter the value: "))
+y=int(input("enter the value: "))
+m=int(input("enter the value of salary: "))
+k=(m*y)/x
+print(k)
+z=int(input("enter the value: "))
+print((1/k)*z))
