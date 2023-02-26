e1eb2ef383c24f1b0dba231c9d9c5dcfd263f578
shivanivippaheli11 <104315622+shivanivippaheli11@users.noreply.github.com>
2023-02-26 23:26:33 +0530

    Create pr2.py

---
+++ pr2.py
@@ -0,0 +1,13 @@
+Paragraph reverse each word in python using recursion
+def reverse(n):
+    if len(n)==0:
+        return " "
+    else:
+        k=n.split()
+        reversed_firstword=k[0][::-1]
+        restofn=(" ".join(k[1:]))
+        reverse_restofn=reverse(restofn)
+        return  reversed_firstword+" "+ reverse_restofn
+n=input("enter the paragraph: ")
+print(reverse(n))
+
