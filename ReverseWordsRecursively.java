998f951a0f8d774b1e3ca652d2f42ccd26b74aee
shivanivippaheli11 <104315622+shivanivippaheli11@users.noreply.github.com>
2023-03-03 22:07:44 +0530

    Update ReverseWordsRecursively.java

--- ReverseWordsRecursively.java
+++ ReverseWordsRecursively.java
@@ -16,7 +16,7 @@ public class ReverseWordsRecursively {
         } else {
             String[] words = paragraph.split("\\s+");
             String reversedFirstWord = new StringBuilder(words[0]).reverse().toString();
-            String restOfParagraph = String.join(" ",java.util.Arrays.copyOfRange(words, 1, words.length));
+            String restOfParagraph = String.join(" ",Arrays.copyOfRange(words, 1, words.length));
             String reversedRestOfParagraph = reverseWords(restOfParagraph);
             return reversedFirstWord + " " + reversedRestOfParagraph;
         }
